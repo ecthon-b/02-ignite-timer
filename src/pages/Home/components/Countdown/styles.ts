bce4cda6b2d42d88700a23e6d85d040110ce4523
ecthon <ecthon@gmail.com>
2022-08-29 19:04:19 -0400

    Funcionalidades da aplicação - Prop Drilling no React

--- src/pages/Home/components/Countdown/styles.ts
+++ src/pages/Home/components/Countdown/styles.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-
 export const CountdownContainer = styled.div`
   font-family: 'Roboto Mono', monospace;
   font-size: 10rem;
--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,35 +1,56 @@
 import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+import * as zod from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 05 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function NewCycleForm() {
-    return (
-        <FormContainer>
-            <label htmlFor="task">Vou trabalhar em</label>
-            <TaskInput
-                id="task"
-                list="task-suggestions"
-                placeholder="Dê um nome para seu projeto"
-                disabled={!!activeCycle}
-                {...register('task')}
-            />
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
 
-            <datalist id="task-suggestions">
-                <option value="Estudar react" />
-                <option value="Fazer ezercício do Ignite" />
-            </datalist>
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
 
-            <label htmlFor="minutesAmount">durante</label>
-            <MinutesAmountInput
-                id="minutesAmount"
-                type="number"
-                placeholder="00"
-                step={5}
-                min={5}
-                max={60}
-                disabled={!!activeCycle}
-                {...register('minutesAmount', { valueAsNumber: true })}
-            />
+      <datalist id="task-suggestions">
+        <option value="Estudar react" />
+        <option value="Fazer ezercício do Ignite" />
+      </datalist>
 
-            <span>minutos.</span>
-        </FormContainer>
-    )
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        id="minutesAmount"
+        type="number"
+        placeholder="00"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
 }
